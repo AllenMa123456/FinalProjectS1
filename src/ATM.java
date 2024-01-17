79ff3604f98acd58c322dafaf8ce4212b8403bcc
Allen <allenm2789@nycstudents.net>
2024-01-17 10:48:38 -0500

    Started main menu

--- src/ATM.java
+++ src/ATM.java
@@ -6,6 +6,7 @@ public class ATM {
     private Account checkAccount;
     private Customer user;
     private boolean access;
+    private boolean quit = false;
 
 
 
@@ -67,6 +68,7 @@ public class ATM {
         }
         System.out.println("Access granted.");
         access = true;
+        mainMenu();
 
     }
     private boolean checkPinContents(String thePin) {
@@ -82,8 +84,58 @@ public class ATM {
     }
 
     private void mainMenu() {
-        while (access) {
+        int option;
+        while (access && !quit) {
+            System.out.println();
+            System.out.println("Please select an option.");
+            System.out.println("1. Withdraw money");
+            System.out.println("2. Deposit money");
+            System.out.println("3. Transfer Money between Accounts");
+            System.out.println("4. Get Account Balances");
+            System.out.println("5. Get Transaction History");
+            System.out.println("6. Change PIN");
+            System.out.println("7. Exit");
+            System.out.println();
+            System.out.print("Option: ");
+            option = scan.nextInt();
+            if (option == 1) {
+                // Still need to implement $5 and $20 function
+                System.out.println();
+                System.out.println("Which account would you like to withdraw money from? (Enter the number)");
+                System.out.println("1. Savings Account");
+                System.out.println("2. Checking Account");
+                int option2 = scan.nextInt();
+                if (option2 == 1) {
+                    System.out.print("How much money would you like to withdraw from your Savings Account?: $");
+                    int money = scan.nextInt();
+                    saveAccount.withdrawMoney(money);
+                } else {
+                    System.out.print("How much money would you like to withdraw from your Checking Account?: $");
+                    int money = scan.nextInt();
+                    checkAccount.withdrawMoney(money);
+                }
+            } else if (option == 2) {
+                System.out.println();
+                System.out.println("Which account would you like to deposit money into? (Enter the number)");
+                System.out.println("1. Savings Account");
+                System.out.println("2. Checking Account");
+                int option2 = scan.nextInt();
+                if (option2 == 1) {
+                    System.out.print("How much money would you like to deposit into your Savings Account?: $");
+                    int money = scan.nextInt();
+                    saveAccount.depositMoney(money);
+                } else {
+                    System.out.print("How much money would you like to deposit into your Checking Account?: $");
+                    int money = scan.nextInt();
+                    checkAccount.depositMoney(money);
+                }
+            } else if (option == 3) {
 
+            } else if (option == 4) {
+                System.out.println();
+                System.out.println("Savings Account: " + saveAccount.getMoney());
+                System.out.println("Checking Account: " + checkAccount.getMoney());
+            }
         }
     }
 
